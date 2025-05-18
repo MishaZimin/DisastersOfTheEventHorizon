a3b6c309816b022e77818bbd41e6baabbb926c39
ZiminMisha <mikhail.zimin.2004@bk.ru>
2025-05-18 18:59:34 +0500

    feat: add menu

--- Assets/Scenes/Game/Scripts/PlayerStats.cs
+++ Assets/Scenes/Game/Scripts/PlayerStats.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerStats : MonoBehaviour {
+public class PlayerStats : MonoBehaviour
+{
+    public static int Money;
+    public int startMoney = 300;
 
-	public static int Money;
-	public int startMoney = 300;
+    public static int Lives;
+    public int startLives = 10;
 
-	public static int Lives;
-	public int startLives = 10;
+    [Header("Passive Income")]
+    public int incomePerSecond = 5;
 
-	void Start ()
-	{
-		Money = startMoney;
-		Lives = startLives;
-	}
+    void Start()
+    {
+        Money = startMoney;
+        Lives = startLives;
 
+        StartCoroutine(AddMoneyOverTime());
+    }
+
+    IEnumerator AddMoneyOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            Money += incomePerSecond;
+        }
+    }
 }
